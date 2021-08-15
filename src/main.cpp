a9ac67917fe0688e5a867fbc906e79ffe42b306e
RazdolbayOne <Yone312@gmail.com>
2021-08-15 11:40:19 +0300

    refactor: change logic and name

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,51 @@
 
 
 
-bool get_name();
+const char* get_names( char* str,  char* card_number) {
+	char* pch;
+	pch =  strchr(str, ';');
+	while (pch != NULL)
+	{
+		printf("%c ", *pch);
+		printf("found at %lld\n", pch - str + 1);
+		pch = strchr(pch + 1, ';');
+	}
+	printf("\n");
+
+	return NULL;
+}
+
+
+
+
 
 //searches card number in files data
 //if found returns true and Name of card user
 //otherwise false
-bool search_card_num(char file_name[],char card_number[]) {
+bool get_name(const char file_name[],  char card_number[]) {
 
 	FILE* pfile;
 	const size_t buff_size = 255;
-
 	char buff[buff_size];
 
-	char* pch;
 
 	fopen_s(&pfile, file_name, "rt");
 
-	if (pfile != NULL) {
+	if (pfile == NULL) {
+
+		printf("cannot find file \"%s\"", file_name);
+		return false;
+	}
+	else{
+
 		while (fgets(buff, buff_size, pfile) != NULL) {
 			printf("%s", buff);
 
-			pch = strchr(buff, ';');
-			while (pch!=NULL)
-			{
-				printf("%c ", *pch);
-				printf("found at %d\n", pch - buff + 1);
-				pch = strchr(pch + 1, ';');
-			}
-			printf("\n");
+			printf("%s", get_names(buff, card_number));
 		}
 
 		fclose(pfile);
 		return true;
-
-	}
-	else
-	{
-		printf("cannot find file \"%s\"", file_name);
-		return false;
 	}
 
 }
@@ -52,7 +59,7 @@ int main() {
 	char card_number[16] = "1564683215646"; //just random number
 	char card_number1[16] = "50000001"; //just random number
 	char card_number2[16] = "4000"; //just random number
-	search_card_num(file_name, card_number);
+	get_name(file_name, card_number);
 
 
 	return 0;
