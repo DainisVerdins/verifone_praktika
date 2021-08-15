34b0f8f7abbbe0cf7c33ed4b17c3ce7c24820bf0
RazdolbayOne <Yone312@gmail.com>
2021-08-15 23:03:19 +0300

    feat:Check if input card number is valid
    checks if inputed card number is valid for further interactions

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
-#include<cstdio>
+#include<stdio.h>
 #include <cstring>//pause
+#include <cctype>//isdigit
 
 
 
-const char* get_names( char* str,  char* card_number) {
+char* get_names(char* str, char* card_number)
+{
+
+	char range_start[16];
+	char range_end[16];
+
+	const size_t buff_size = 255;
+	char buff[buff_size];//buffer what contains name;
+
+
 	char* pch;
-	pch =  strchr(str, ';');
+	pch = strchr(str, ';'); //TODO: make separator sign as macros
 	while (pch != NULL)
 	{
 		printf("%c ", *pch);
@@ -13,8 +23,8 @@ const char* get_names( char* str,  char* card_number) {
 		pch = strchr(pch + 1, ';');
 	}
 	printf("\n");
-
-	return NULL;
+	char dummy[] = "Wow!";
+	return card_number;
 }
 
 
@@ -24,11 +34,14 @@ const char* get_names( char* str,  char* card_number) {
 //searches card number in files data
 //if found returns true and Name of card user
 //otherwise false
-bool get_name(const char file_name[],  char card_number[]) {
+char* get_name(const char file_name[], char card_number[])
+{
 
 	FILE* pfile;
+	char* Name;
 	const size_t buff_size = 255;
-	char buff[buff_size];
+	Name = NULL;
+	char buff[255];
 
 
 	fopen_s(&pfile, file_name, "rt");
@@ -36,30 +49,61 @@ bool get_name(const char file_name[],  char card_number[]) {
 	if (pfile == NULL) {
 
 		printf("cannot find file \"%s\"", file_name);
-		return false;
+		return NULL;
 	}
-	else{
+	else {
 
 		while (fgets(buff, buff_size, pfile) != NULL) {
 			printf("%s", buff);
 
-			printf("%s", get_names(buff, card_number));
+			Name = get_names(buff, card_number);
 		}
 
 		fclose(pfile);
-		return true;
+		return Name;
 	}
 
 }
 
+//check if card_number have valid specifics 
+bool is_valid(const char card_number[])
+{
+	int card_length = strlen(card_number);
+	int i = 0;
 
-int main() {
-	printf("Hello World!\n");
+	//card number must precisly 16 digits
+	const int chars_in_card = 16;//probably to need to add as macros
+
+	if (card_length< chars_in_card || card_length>chars_in_card) 
+	{
+		return false;
+	}
+
+	for (i = 0; i < card_length; ++i)
+	{
+		if (!isdigit(card_number[i]))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+int main()
+{
 	char file_name[] = "file.txt";
-	char card_number[16] = "1564683215646"; //just random number
-	char card_number1[16] = "50000001"; //just random number
-	char card_number2[16] = "4000"; //just random number
-	get_name(file_name, card_number);
+	char card_number[] = "1234567890123456"; //just random number
+
+	if (is_valid(card_number))
+	{
+		puts(card_number);
+
+	}
+	else {
+		printf("bad cards number -> %s\n", card_number);
+	}
 
 
 	return 0;
